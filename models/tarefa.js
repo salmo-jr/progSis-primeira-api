5b84d658146ad76ea540a470ed572e633df3e6c9
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-06-24 19:35:38 -0300

    migrations

---
+++ models/tarefa.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tarefa extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Tarefa.init({
+    nome: DataTypes.STRING,
+    desc: DataTypes.STRING,
+    status: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Tarefa',
+  });
+  return Tarefa;
+};
