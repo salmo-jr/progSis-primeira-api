a16b4116c8ee17eed3ee1e61a3956a8ccd340e11
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-08-04 17:47:31 -0300

    revert to stable version

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,40 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const productsRouter = require('./products.routes');
 const usersRouter = require('./users.routes');
-const sessionsRouter = require('./sessions.routes');  
+const sessionsRouter = require('./sessions.routes');
 
 const routes = express.Router();
+
+function ensureAuthenticated(request, response, next) {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+        return response.status(403).json({
+            message: 'Token inexistente'
+        });
+    }
+    
+    /*
+    const auth = authHeader.split(' ');
+    const type = auth[0];
+    const token = auth[1];
+    */
+    const [, token] = authHeader.split(' ');
+    try {
+        jwt.verify(token, 'secret_key');
+        return next();
+    } catch (error) {
+        return response.status(403).json({
+            message: 'Token inválido'
+        });
+    }
+}
+
+routes.use(cors());
+routes.use('/sessions', sessionsRouter);
+routes.use(ensureAuthenticated);
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
 
 module.exports = routes;
