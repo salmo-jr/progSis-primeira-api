578ded3ab1493f4fbbe2155f623732b3d9f00bd3
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-08-04 17:01:57 -0300

    test route

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
+const { request, response } = require('express');
 const express = require('express');
 const router = require('../src/routes');
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.get('/test', (request, response) => {
+    return response.json({ success: true });
+});
 
 app.listen(3333, () => {
     console.log('Server started on port 3333!');
