5a8952f1c688def5a7ee6dedfd0ded14a54588ad
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-08-04 19:41:08 -0300

    resets the api

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = require('../src/routes');
+const router = require('./routes');
 const app = express();
 
 app.use(express.json());
-//app.use(router);
+app.use(router);
 
 app.get('/', (request, response) => {
     return response.json({ success: true });
